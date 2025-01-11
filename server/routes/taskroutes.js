0be2e085f75bf9b9e547fec6ca3935b074dd027c
AdarshMasekar <adarshmasekar@gmail.com>
2025-01-11 20:39:19 +0530

    Add task and user routes for API operations
    - Created taskroutes.js to handle CRUD operations for tasks, including endpoints for getting, creating, updating, and deleting tasks with authentication middleware.
    - Created userroutes.js to manage user signup and signin processes, including validation and user retrieval.
    - Integrated middleware for task and user operations to ensure proper request handling and validation.
    - Enhanced API structure by organizing routes for better maintainability and scalability.

---
+++ server/routes/taskroutes.js
@@ -0,0 +1,61 @@
+const {Router} = require("express")
+const router = Router();
+const taskMiddeware = require("../middleware/taskMiddleware")
+const authMiddleware = require("../middleware/authMiddleware");
+const { getTask,getTasks,createTask,deleteTask,updateTask } = require("../controller/taskController");
+
+router.get("/",authMiddleware,async(req,res)=>{
+    const userId = req.user.userId;
+    const tasks = await getTasks(userId);
+    if(!tasks.success){
+        return res.status(400).json({"error":tasks.error})
+    }
+    res.status(200).json(tasks.tasks)
+})
+
+router.get("/:taskId",authMiddleware,async(req,res)=>{
+    const userId = req.user.userId;
+    const taskId = req.params.taskId;
+    const task = await getTask(userId,taskId);
+    if(!task.success){
+        return res.status(400).json({"error":task.error})
+    }
+    res.status(200).json(task.task)
+})
+
+router.post("/",authMiddleware,taskMiddeware,async(req,res)=>{
+    const title = req.body.title;
+    const description = req.body.description;
+    const priority = req.body.priority;
+    const deadLine = req.body.deadLine;
+    const subtasks = req.body.subtasks;
+    const userId = req.user.userId;
+
+    const newTask = await createTask({title,description,priority,subtasks,deadLine,userId});
+    console.log(newTask)
+    if(!newTask.success){
+        return res.status(400).json({error:"task creation failed!"})
+    }
+    res.status(201).json(newTask.task)
+})
+
+router.patch("/:taskId",authMiddleware,async(req,res)=>{
+    const taskId = req.params.taskId;
+    const updates = req.body;
+    const updatedTask = await updateTask(taskId,updates);
+    if(!updatedTask.success){
+        return res.status(400).json({error:updatedTask.error})
+    }
+    res.status(201).json(updatedTask)
+})
+
+router.delete("/:taskId",authMiddleware,async(req,res)=>{
+    const taskId = req.params.taskId;
+    const deletedTask = await deleteTask(taskId);
+    if(!deletedTask.success){
+        return res.status(400).json({error:deletedTask.error})
+    }
+    res.status(200).json(deletedTask.deletedTask)
+})
+
+module.exports = router
---
+++ server/routes/userroutes.js
@@ -0,0 +1,42 @@
+const {Router} = require("express");
+const router = Router();
+const User = require("../model/User");
+const SALT_ROUNDS = require("../config/dotenv")
+const signupMiddleware = require("../middleware/signupMiddleware")
+const signinMiddleware = require("../middleware/signinMiddleware")
+const {createUser,validate} = require("../controller/userController")
+
+
+router.post("/signup",signupMiddleware,async(req,res)=>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = await createUser({firstName,lastName,email,password});
+    if(!newUser.success){
+        return res.status(400).json({error:"user creation failed!"})
+    }
+    res.status(201).json({msg:"user registered successfully!"})
+})
+
+router.post("/signin",signinMiddleware,async(req,res)=>{
+    const password = req.body.password;
+    const user = req.user;
+
+    const validateUser = await validate(user,password);
+    if(!validateUser.success){
+        return validateUser.error;
+    }
+    const token = validateUser.token;
+    console.log(token)
+    res.status(200).json({token:token})
+})
+
+router.get("/",async(req,res)=>{
+    const users = await User.find();
+    res.json({users:users})
+})
+
+
+module.exports = router;
