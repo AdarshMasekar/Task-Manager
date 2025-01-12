ef078853d98349c3ca9e081351f6222836bedb7d
AdarshMasekar <adarshmasekar@gmail.com>
2025-01-12 20:25:30 +0530

    Implement Error Boundary for improved error handling in the application
    - Added ErrorBoundary component to wrap the main application routes, enhancing user experience by catching errors in the UI.
    - Integrated Fallback component to provide a user-friendly error message when an error occurs, ensuring better error management and feedback for users.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Navbar from "./components/layout/Navbar"
 import Home from "./pages/Home"
 import Dashboard from "./pages/Dashboard"
+import { ErrorBoundary } from "react-error-boundary";
+import { Fallback } from "./components/ui/Fallback";
 
 function App() {
 
@@ -9,11 +11,13 @@ function App() {
         <div className="px-40">
             <Router>
             <Navbar/>
+            < ErrorBoundary FallbackComponent={Fallback}>
             <Routes>
                 <Route path="/" element={<Dashboard/>} />
                 {/* <Route path="/dashboard" element={<Dashboard/>}/>
                 <Route path="/signup" element={<SignUp/>} /> */}
             </Routes>
+            </ErrorBoundary>
         </Router>
         </div>
   )
