c4e41489f65088d69c437a921e7c1985fc964620
AdarshMasekar <adarshmasekar@gmail.com>
2025-01-13 18:49:19 +0530

    Remove user-related Redux store and reducer files
    - Deleted the store configuration in store.jsx and the userReducer.js file to streamline the state management structure.
    - This change eliminates unused code and prepares for a potential refactor or replacement of the user management logic.

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './redux/reduceres/userReducer';
+import userReducer from '../redux/reducers/userReducer';
 
 const store = configureStore({
   reducer: {
