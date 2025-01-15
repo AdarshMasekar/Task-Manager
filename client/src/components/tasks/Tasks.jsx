c8d4bc01c0a52bdb30fff591673855a5065bd703
AdarshMasekar <adarshmasekar@gmail.com>
2025-01-15 23:02:36 +0530

    Enhance Tasks component with delete functionality and UI improvements
    - Integrated task deletion feature using removeTaskAsync, with a success notification upon deletion.
    - Updated task progress calculation to handle cases with no subtasks.
    - Improved UI layout for better readability and user experience, including adjustments to the action buttons and task details display.
    - Added ToastContainer for user feedback on task deletion.

--- client/src/components/tasks/Tasks.jsx
+++ client/src/components/tasks/Tasks.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { fetchTasks, taskSelector } from '../../store/reducers/taskReducer';
+import { fetchTasks, taskSelector, removeTaskAsync } from '../../store/reducers/taskReducer';
 import { useDispatch } from 'react-redux';
 import {
     Card,
@@ -14,23 +14,28 @@ import {
 } from "@material-tailwind/react";
 import { Link } from 'react-router-dom';
 import { PencilIcon } from "@heroicons/react/24/solid";
-
+import { CheckIcon } from "@heroicons/react/24/outline";
 import { CalendarDaysIcon } from "@heroicons/react/24/outline";
 import { ListBulletIcon } from "@heroicons/react/24/outline";
 import { FlagIcon } from "@heroicons/react/24/outline";
-
-
-
-
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Tasks = () => {
     const { tasks } = useSelector(taskSelector);
     const dispatch = useDispatch();
+    const notify = () => toast("task deleted successfully");
 
     useEffect(() => {
         dispatch(fetchTasks());
-    }, [dispatch]);
+    }, []);
+
+    const handleDelete = (taskId) => {
+        dispatch(removeTaskAsync(taskId));
+        notify();
+    };
 
 
     return (
@@ -38,37 +43,42 @@ const Tasks = () => {
             {tasks && (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {tasks.map(({ _id, title, description, priority, subtasks, deadLine }) => {
-                        const completedSubtasks = subtasks.filter(task => task.status === "Completed").length;
-                        const totalSubtasks = subtasks.length;
+                        const completedSubtasks = subtasks ? subtasks.filter(task => task.status === "Completed").length : 0;
+                        const totalSubtasks = subtasks ? subtasks.length : 0;
                         const progress = totalSubtasks > 0 ? (completedSubtasks / totalSubtasks) * 100 : 0;
                         return (
                             <Card key={_id} className="dark:bg-gray-800 dark:border-gray-700 shadow-md ">
                                 <CardBody>
-                                    <div className="flex flex-col items-start gap-2 mb-4">
-                                        <Chip
-                                            value={priority}
-                                            icon={<FlagIcon className="h-6 w-6 pe-1 pb-1 me-1 mb-1" />}
-                                            variant="ghost"
-                                            color={
-                                                priority === "High"
-                                                    ? "red"
-                                                    : priority === "Medium"
-                                                        ? "yellow"
-                                                        : "green"
-                                            }
-                                        />
-                                        <Typography variant="h4" color="blue-gray" className="dark:text-white font-bold">
-                                            {title}
-                                        </Typography>
+                                    <div className="flex justify-between items-start mb-4">
+                                        <div className="flex flex-col items-start gap-2">
+                                            <Chip
+                                                value={priority}
+                                                icon={<FlagIcon className="h-6 w-6 pe-1 pb-1 me-1 mb-1" />}
+                                                variant="ghost"
+                                                color={
+                                                    priority === "High"
+                                                        ? "red"
+                                                        : priority === "Medium"
+                                                            ? "yellow"
+                                                            : "green"
+                                                }
+                                            />
+                                            <Typography variant="h4" color="blue-gray" className="dark:text-white font-bold">
+                                                {title}
+                                            </Typography>
+                                        </div>
                                     </div>
                                     <Typography variant='h6' className=" dark:text-gray-300 text-base mb-4">
                                         {description}
                                     </Typography>
                                     <div className="flex justify-between items-center mb-2">
                                         <div className="progress flex gap-2">
-                                            <ListBulletIcon className="h-6 w-6 text-gray-500" />
+                                            {completedSubtasks === totalSubtasks ?
+                                                <CheckIcon className="h-6 w-6 bg-green-500 rounded-full text-white p-1" /> :
+                                                <ListBulletIcon className="h-6 w-6 text-gray-500" />
+                                            }
                                             <Typography variant="h4" color="gray" className=" dark:text-gray-400 text-sm">
-                                                    Progress
+                                                Progress
                                             </Typography>
                                         </div>
                                         <Typography variant="h6" color="gray" className="dark:text-gray-400 text-sm">
@@ -76,12 +86,12 @@ const Tasks = () => {
                                         </Typography>
                                     </div>
                                     <Progress value={progress} color={
-                                                priority === "High"
-                                                    ? "red"
-                                                    : priority === "Medium"
-                                                        ? "yellow"
-                                                        : "green"
-                                            }  className="mb-4 shadow-inner shadow-gray-300 inset-1 h-2" />
+                                        priority === "High"
+                                            ? "red"
+                                            : priority === "Medium"
+                                                ? "yellow"
+                                                : "green"
+                                    } className="mb-4 shadow-inner shadow-gray-300 inset-1 h-2" />
 
                                     <div className="flex items-center bg-secondary rounded-md w-fit px-2 py-1 mb-2 gap-2">
 
@@ -111,15 +121,37 @@ const Tasks = () => {
                                             </details>
                                         </div>
                                     )}
-
-                                    <Link to={`/edit-task/${_id}`} state={{ task: { _id, title, description, priority, subtasks, deadLine } }}>
-                                        <Button variant="gradient" color="black" className="flex items-center gap-2 mt-4">
-                                            Edit
-                                            <PencilIcon className="h-4 w-4" />
+                                    <div className="action-items flex justify-between">
+                                        <Link to={`/edit-task/${_id}`} state={{ task: { _id, title, description, priority, subtasks, deadLine } }} >
+                                            <Button variant="gradient" color="blue" className="flex items-center gap-4 mt-4">
+                                                Edit
+                                                <PencilIcon className="h-4 w-4" />
+                                            </Button>
+                                        </Link>
+                                        <Button
+                                                variant="gradient"
+                                                color="red"
+                                                onClick={() => handleDelete(_id)}
+                                                className="flex items-center gap-4 mt-4">
+                                                Delete
+                                                <TrashIcon className="h-4 w-4" />
                                         </Button>
-                                    </Link>
+                                    </div>
                                 </CardBody>
+                                <ToastContainer
+                                    position="bottom-right"
+                                    autoClose={4000}
+                                    hideProgressBar={false}
+                                    newestOnTop={false}
+                                    closeOnClick={true}
+                                    rtl={false}
+                                    pauseOnFocusLoss
+                                    draggable
+                                    pauseOnHover
+                                    theme="light"
+                                />
                             </Card>
+
                         );
                     })}
                 </div>
