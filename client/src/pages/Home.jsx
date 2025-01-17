77b533e4a81f009104849c4bd5b202db3a64306f
AdarshMasekar <adarshmasekar@gmail.com>
2025-01-17 22:47:37 +0530

    Refactor Homepage component for improved UI and styling
    - Updated background color for better visual consistency.
    - Enhanced button and link styles to align with primary theme.
    - Improved icon styling for better integration with the overall design.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,16 +4,16 @@ import { PlusIcon } from "@heroicons/react/16/solid";
 
 export default function Homepage() {
   return (
-    <div className="container mx-auto mt-8 p-4">
+    <div className="bg-background mx-auto mt-8 p-4">
       <div className="flex justify-between items-center mb-6">
         <h2 className="text-2xl font-semibold text-gray-900 dark:text-white">
           Task List
         </h2>
-        <Link to="/add-task" className='bg-blue-500 flex items-center justify-center px-2 py-1 rounded-md'>
-          <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-primary-500 dark:bg-primary-700 dark:hover:bg-primary-800">
+        <Link to="/add-task" className='bg-primary flex items-center justify-center px-2 py-1 rounded-lg'>
+          <button className="px-4 py-2 bg-primary text-white border-none">
             Add Task
           </button>
-        <PlusIcon className="h-6 w-6 text-white" />
+        <PlusIcon className="h-6 w-6 bg-primary text-white" />
         </Link>
       </div>
       <ViewTasks />
