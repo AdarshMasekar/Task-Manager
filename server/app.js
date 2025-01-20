15fed126408cd0590b3f5c8ac4da78ff0336c9c1
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2025-01-20 20:49:27 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://task-manager-frontend-smoky.vercel.app",
     credentials: true
 }));
 app.use(express.json());
