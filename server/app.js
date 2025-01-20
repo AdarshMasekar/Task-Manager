13eda7fe7025b22023eb62f2029b788f7e7351e9
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2025-01-20 20:08:59 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const app = express();
-const TaskRoutes = require("./routes/taskroutes")
-const UserRoutes = require("./routes/userroutes")
-const cors  = require("cors")
+const TaskRoutes = require("./routes/taskroutes");
+const UserRoutes = require("./routes/userroutes");
+const cors = require("cors");
 require('dotenv').config();
 
 app.use(cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials:true
-}))
+    credentials: true
+}));
 app.use(express.json());
-app.use("/api/tasks",TaskRoutes)
-app.use("/api/user",UserRoutes)
+app.use("/api/tasks", TaskRoutes);
+app.use("/api/user", UserRoutes);
 
+app.get("/", (req, res) => {
+    res.send("welcome to task manager backend");
+});
 
-app.get("/",(req,res)=>{
-    res.json({msg:"welcome to task manager backend"})
-})
-
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     if (err) {
         console.error("Error occurred:", err.stack);
-        // Check if it's a 404 or a general server error
         res.status(err.statusCode || 500).json({
             success: false,
             message: err.message || "Internal Server Error"
@@ -29,12 +27,6 @@ app.use((err,req,res,next)=>{
     } else {
         next();
     }
-})
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
