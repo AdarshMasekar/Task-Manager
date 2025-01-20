26a3984105251a1f2c859311e32b0fc612bd5f56
AdarshMasekar <adarshmasekar@gmail.com>
2025-01-20 16:10:43 +0530

    Enhance ToastContainer configuration in App component for improved user experience
    - Updated ToastContainer properties to customize position, auto-close duration, and appearance.
    - Ensured toast notifications are more user-friendly with new settings for better visibility and interaction.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,19 @@ function App() {
   return (
     <BrowserRouter>
         <Navbar/>
-        < ToastContainer />
+        <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            className="w-full sm:w-auto"
+        />
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/signup" element={<Register/>} />
